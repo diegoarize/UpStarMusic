f02ef6dc4f172a1605f443c7d1b432619b452314
diegoarize <diegoarize@outlook.com>
2017-02-28 11:26:45 -0300

    Creating the models

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,30 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required.']
+  },
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: {
+    type: String,
+    validate: {
+      validator: (image) => isUrlValid(image) ,
+      message: 'image url must be valid.'
+    }
+  },
+  revenue: Number
+});
+
+export var isUrlValid = (url) => {
+  var res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  if(res == null)
+      return false;
+  else
+      return true;
+}
+
+module.exports = AlbumSchema;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,42 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = require('./album');
+
+mongoose.Promise = global.Promise;
+
+const ArtistSchema = new Schema({
+  name: {
+    type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters'
+    },
+    required: [true, 'Name is required.']
+  },
+  age: Number,
+  yearsActive: Number,
+  image: {
+    type: String,
+    validate: {
+      validator: (image) => isUrlValid(image) ,
+      message: 'image url must be valid.'
+    }
+  },
+  genre: String,
+  website: {
+    type: String,
+    validate: {
+      validator: (website) => isUrlValid(website) ,
+      message: 'website url must be valid.'
+    }
+  },
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = Artist;
