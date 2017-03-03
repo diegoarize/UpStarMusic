dce515abbe735420ce7a24e0beb01366dc8bf6f1
diegoarize <diegoarize@outlook.com>
2017-03-02 21:21:10 -0300

    implementing search artists query

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,10 +9,12 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-  var query = Artist.find({})
+  var query = Artist.find(buildQuery(criteria))
     .sort({[sortProperty]: 1})
     .skip(offset)
     .limit(limit);
+
+    console.log(criteria);
 
     return Promise.all([query, Artist.count()])
       .then((results) => {
@@ -24,4 +26,30 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
         };
       });
 
+};
+
+var buildQuery = (criteria) => {
+  const query = {};
+
+  if(criteria.name) {
+    query.$text = {
+      $search: criteria.name
+    };
+  }
+
+  if(criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+
+  if(criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+
+  return query;
 };
